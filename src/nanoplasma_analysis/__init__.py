32890d994c5108c63d987d8f8f54b40e3e59c51d
Cemedenh <cemedenh@example.com>
2026-06-22 14:47:48 +0200

    unzippt

---
+++ src/nanoplasma_analysis/__init__.py
@@ -0,0 +1,5 @@
+"""Nanoplasma analysis helpers for PIConGPU openPMD outputs."""
+
+from .core import NanoPlasmaRun, extract_step_from_filename
+
+__all__ = ["NanoPlasmaRun", "extract_step_from_filename"]
